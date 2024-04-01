1a9023bbbfc18b3b2f5331ea02fa207ffb502f46
Moli-M <diegomolinero842@gmail.com>
2024-04-01 13:52:37 +0200

    Add register endpoint

--- app/app.py
+++ app/app.py
@@ -1,9 +1,22 @@
 import os
-from flask import Flask, redirect, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import db, User
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'test'
 
+db.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def index():
@@ -15,38 +28,33 @@ def index():
     }
     return render_template('index.html', data=data)
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password)
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html')
 
 @app.route('/login')
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            return 'Login fallido. Por favor, verifica tus credenciales.'
     data={
         'titulo': 'Login',
     }
     return render_template('login.html', data=data)
-
-@app.route('/contacto')
-def contacto():
-    data={
-        'titulo': 'Contacto',
-        'encabezado': 'Prueba'
-    }
-    return render_template('contacto.html', data=data)
-
-@app.route('/suma/<int:v1>/<int:v2>')
-def suma(v1,v2):
-    return '{}'.format(v1 + v2)
-
-@app.route('/datos')
-def datos():
-    valor1 = request.args.get('v1') 
-    return f'LADjfalñsdjflk {valor1}'
-
-@app.route('/lenguajes')
-def lenguajes():
-    data={
-        'titulo': 'Lenguajes',
-        'lenguajes':['PHP', 'Python', 'C', 'Java']
-    }
-    return render_template('lenguajes.html', data=data)
 
 @app.route('/upload')
 def upload():
